1489056af6f343585d97bdf8dfa8efaa39b2743d
AbuSuudy <abubakar.suudy@gmail.com>
2023-06-17 16:57:22 +0100

    Twitter V2 changes

--- AfricanObjects/Interface/ITweetService.cs
+++ AfricanObjects/Interface/ITweetService.cs
@@ -10,7 +10,7 @@ namespace AfricanObjects.Interface
         public Task<bool> UploadImage(string imageURL, CancellationToken token);
         public Task<MuseumObject> StartTweeting(CancellationToken token);
         public Task<bool> TweetText(string text, CancellationToken token);
-        public Task<bool> SendText(string URL, Dictionary<string, string> textData, CancellationToken token);
+        public Task<bool> SendText(string URL, MediaTweet media, CancellationToken token);
         public string PrepareOAuth(string URL, Dictionary<string, string> data, string httpMethod, CancellationToken token);
         public string GenerateSignature(string url, Dictionary<string, string> data, string httpMethod, CancellationToken token);
         public string GenerateOAuthHeader(Dictionary<string, string> data, CancellationToken token);
--- AfricanObjects/Models/TweetObject.cs
+++ AfricanObjects/Models/TweetObject.cs
@@ -13,4 +13,16 @@ namespace AfricanObjects.Models
         public string Country { get; set; }
         public string Source { get; set; }
     }
+
+
+    public class Media
+    {
+        public List<string> media_ids { get; set; }
+    }
+
+    public class MediaTweet
+    {
+        public string text { get; set; }
+        public Media media { get; set; }
+    }
 }
